1955b3db475f821703f6e6bd23225c1168fecf0e
jongpants <jong.lau99@gmail.com>
2021-10-10 21:50:48 +0300

    v.2.0

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -10,6 +10,9 @@ import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.Category;
 import com.example.Bookstore.domain.CategoryRepository;
+import com.example.Bookstore.domain.User;
+import com.example.Bookstore.domain.UserRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,7 +23,7 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			crepository.save(new Category("Fantasy"));
@@ -28,7 +31,14 @@ public class BookstoreApplication {
 			crepository.save(new Category("Adventure"));
 			
 			brepository.save(new Book("Ernest Hemingway", "A Farewell to Arms", "1232323-21", 1929, crepository.findByName("Adventure").get(0)));
-			brepository.save(new Book("George Orwell", "Animal Farm", "2212343-5", 1945, crepository.findByName("Horror").get(0)));	
+			brepository.save(new Book("George Orwell", "Animal Farm", "2212343-5", 1945, crepository.findByName("Horror").get(0)));
+			
+			User user1 = new User("user", "user@usermail.com", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "admin@adminmail,com", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			User user3 = new User("jong", "jong.lau99@gmail.com", "$2a$10$rAXY/apSPNgR11EVSGYAO.DFDa3wGEBcTho2AAJQ/3WnoUc1Uql7i", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			urepository.save(user3);
 			
 			log.info("fetch all books");
 			for (Book book : brepository.findAll()) {
